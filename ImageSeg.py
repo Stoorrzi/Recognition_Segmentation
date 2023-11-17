42893cc001e254ef62f0059e57e443b4adb7479e
Philipp Storz <philipp.storz@t-online.de>
2023-11-17 18:37:51 +0100

    update

--- ImageSeg.py
+++ ImageSeg.py
@@ -6,84 +6,110 @@ import numpy as np
 import time
 
 start_time = time.time()
-model = YOLO('yolov8s-seg.pt')
+model = YOLO("yolov8s-seg.pt")
 
 video_path = "Vid_3.mov"
 cap = cv2.VideoCapture(video_path)
-target_name = ""
+
+b_first_round = True
+n_objects = 0
+n_bbox = []
+
+
+def getPointOfPoints(points):
+    top_left_point = points[0]
+    for point in points:
+        if point[0] < top_left_point[0]:
+            top_left_point = point
+        elif point[0] == top_left_point[0] and point[1] < top_left_point[1]:
+            top_left_point = point
+
+    return top_left_point
+
+
+def getPointBbox(index):
+    bbox = results[0].boxes.xyxy.tolist()[index]
+    print(bbox)
+
+    p1 = (bbox[2] + bbox[0]) / 2
+    p2 = (bbox[3] + bbox[1]) / 2
+
+    center = [p1, p2]
+    # cv2.circle(annotated_frame, (int(p1), int(p2)), 5, (0, 255, 0), 10)
+
+    return center
+
+
+def getPointMask(index):
+    arr = results[0].masks.xy[index]
+    arr1, arr2 = np.split(arr, 2, axis=1)
+
+    arr1 = list(arr1.reshape(-1))
+    arr2 = list(arr2.reshape(-1))
+
+    center = sum(arr1) / len(arr1), sum(arr2) / len(arr2)
+    # cv2.circle(annotated_frame, (int(center[0]), int(center[1])), 5, (255, 0, 0), 10)
+
+    return center
+
+
 while cap.isOpened():
     success, frame = cap.read()
-
     if success:
         results = model(frame)
-        annotated_frame = results[0].plot()     
+        annotated_frame = results[0].plot()
 
-        # Center with bbox
-        if (len(results[0].boxes.xyxy) > 0) :
+        # only workes if object has been detected
+        if len(results[0].boxes.xyxy) > 0:
 
-            target_name = results[0].names[int(results[0].boxes.cls[0])]
+            # first round to set object count
+            if b_first_round:
+                for x in range(len(results[0].boxes.xyxy)):
+                    if results[0].names[int(results[0].boxes.cls[x])] == "spoon":
+                        n_objects += 1
+                b_first_round = False
+            else:
+                local_n_objects = 0
 
-            
+                # count objects in this frame
+                for x in range(len(results[0].boxes.xyxy)):
+                    if results[0].names[int(results[0].boxes.cls[x])] == "spoon":
+                        local_n_objects += 1
+                        n_bbox.append(results[0].boxes.xyxy.tolist()[x])
 
+                # if no further object has been added
+                if local_n_objects <= n_objects:
 
-            bbox=results[0].boxes.xyxy.tolist()[0]
-            print(bbox)
+                    # get target object coordinates
+                    object_target = getPointOfPoints(n_bbox)
 
-            p1 = (bbox[2] + bbox[0])/2
-            p2 = (bbox[3] + bbox[1])/2
-    
-            cv2.circle(annotated_frame, (int(p1), int(p2)), 5, (0, 0, 255), 10)
-        
-        # Center with mask
-        if (len(results[0]) > 0):
-            arr = results[0].masks.xy[0]
-            arr1,arr2=np.split(arr,2,axis=1)
-
-            arr1 = list(arr1.reshape(-1))
-            arr2 = list(arr2.reshape(-1))
-            center = sum(arr1)/len(arr1), sum(arr2)/len(arr2)
-            cv2.circle(annotated_frame, (int(center[0]), int(center[1])), 5, (255, 0, 0), 10)
-        
-        # Center with Mask and bbox compaired
-        if (len(results[0].boxes.xyxy) > 0 and len(results[0]) > 0 ):
-            bbox=results[0].boxes.xyxy.tolist()[0]
-            #print(bbox)
-
-            p1 = (bbox[2] + bbox[0])/2
-            p2 = (bbox[3] + bbox[1])/2
-
-
-            arr = results[0].masks.xy[0]
-            arr1,arr2=np.split(arr,2,axis=1)
-
-            arr1 = list(arr1.reshape(-1))
-            arr2 = list(arr2.reshape(-1))
-            center = sum(arr1)/len(arr1), sum(arr2)/len(arr2)
-
-            x1 = (center[0] + p1) / 2
-            y1 = (center[1] + p2) / 2
-            cv2.circle(annotated_frame, (int(x1), int(y1)), 5, (0, 255, 0), 10)
-
+                    # check object target with all detected objects to get output index
+                    for x in range(len(results[0].boxes.xyxy)):
+                        if results[0].boxes.xyxy.tolist()[x] == object_target:
+                            
+                            # caculate center of the object
+                            center_Mask = getPointMask(x)
+                            center_Bbox = getPointBbox(x)
+                            x1 = (center_Mask[0] + center_Bbox[0]) / 2
+                            y1 = (center_Mask[1] + center_Bbox[1]) / 2
+                            cv2.circle(
+                                annotated_frame,
+                                (int(x1), int(y1)),
+                                5,
+                                (0, 255, 0),
+                                10,
+                            )
         cv2.imshow("YOLOv8 Inference", annotated_frame)
-
+        n_bbox = []
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
         # Break the loop if the end of the video is reached
         break
-
 cap.release()
 cv2.destroyAllWindows()
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-
-
-
-
-
 
 
 # model = FastSAM('./weights/FastSAM-s.pt')  # or FastSAM-x.pt
@@ -99,7 +125,7 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 #         # Bbox default shape [0,0,0,0] -> [x1,y1,x2,y2]
 #         ann = prompt_process.box_prompt(bbox=results[0].boxes.xyxy.tolist()[0])
-        
+
 #         print(prompt_process.results[0].masks[0].xy[0])
 
 #         if (len(prompt_process.results) > 0):
@@ -110,3 +136,18 @@ print("--- %s seconds ---" % (time.time() - start_time))
 #             arr2 = list(arr2.reshape(-1))
 #             center = sum(arr1)/len(arr1), sum(arr2)/len(arr2)
 #             cv2.circle(annotated_frame, (int(center[0]), int(center[1])), 5, (255, 255, 0), 10)
+
+
+# bbox=results[0].boxes.xyxy.tolist()[x]
+#                         print(bbox)
+
+#                         p1 = (bbox[2] + bbox[0])/2
+#                         p2 = (bbox[3] + bbox[1])/2
+
+#                         n_bbox.append([int(p1), int(p2)])
+#                 b_first_round = False
+
+#                 if (n_bbox[0][0] < n_objects[1][0]):
+#                     object_target = "left"
+#                 else:
+#                     object_target = "right"
